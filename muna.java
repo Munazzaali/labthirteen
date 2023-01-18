25c3bc1b53ef986e1aec952b0d941332daf4792b
Munazzaali <110249168+Munazzaali@users.noreply.github.com>
2023-01-18 08:18:31 -0800

    Update muna.java

--- muna.java
+++ muna.java
@@ -3,7 +3,7 @@ public class muna {
 	public static void main(String[] args) {
 
 	    int num = 29;
-	    boolean flag = false;
+	    boolean flag = true;
 	    for (int i = 2; i <= num / 2; ++i) {
 	      // condition for nonprime number
 	      if (num % i == 0) {
